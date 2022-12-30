8d0bb3a1d7510129a510a5a16debbb7bf19ca8b1
SmokeSculpter <117650779+SmokeSculpter@users.noreply.github.com>
2022-12-29 17:11:46 -0800

    yes

---
+++ python/autoMover.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+x=True
+while (x): 
+    pyautogui.moveTo(100, 100, duration=1)
+    time.sleep(30)
+    pyautogui.moveTo(400, 400, duration=1)
+
---
+++ python/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+import os
+import shutil
+import py2exe
+import time
+import pyautogui
+
+setup(windows=['autoMover.py'])
+
